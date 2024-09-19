8d57d49ac86bc3cf9337a3844ce38ecc1097e5fe
webbyart <artkitthana.kh@gmail.com>
2024-09-19 10:54:54 +0700

    Update score.php

--- score.php
+++ score.php
@@ -15,7 +15,8 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+// Set charset to UTF-8
+$conn->set_charset("utf8mb4");
 // Fetch scores
 $sql = "SELECT `name`, `score` FROM `scores` ORDER BY `score` DESC";
 $result = $conn->query($sql);
